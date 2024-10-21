6f1b305be7205ddee5682fc773b37484463531c5
Behzod Xudoyberdiev <behzodjontuit1998@gmail.com>
2024-10-21 22:06:32 +0500

    feat: add users handle functions

--- index.js
+++ index.js
@@ -1,77 +1,93 @@
 import http from 'http';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// In-memory database
 let users = [];
+
+function createUser(request, response) {
+    let body = '';
+    request.on('data', chunk => {
+        body += chunk.toString();
+    });
+    request.on('end', () => {
+        const { username, age, hobbies } = JSON.parse(body);
+        const newUser = { id: uuidv4(), username, age, hobbies };
+        users.push(newUser);
+        response.writeHead(201, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(newUser));
+    });
+}
+
+function getUsers(request, response) {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(users));
+}
+
+
+function getSingleUser(request, response) {
+    const id = request.url.split('/')[2];
+    const user = users.find(u => u.id === id);
+    if (user) {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(user));
+    } else {
+        response.writeHead(404, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify({message: 'User not found'}));
+    }
+}
+
+function updateUser(request, response) {
+    const id = request.url.split('/')[2];
+    let body = '';
+    request.on('data', chunk => {
+        body += chunk.toString();
+    });
+    request.on('end', () => {
+        const index = users.findIndex(u => u.id === id);
+        if (index !== -1) {
+            const { username, age, hobbies } = JSON.parse(body);
+            users[index] = { ...users[index], username, age, hobbies };
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify(users[index]));
+        } else {
+            response.writeHead(404, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({message: 'User not found'}));
+        }
+    });
+}
+
+function deleteUser(request, response) {
+    const id = request.url.split('/')[2];
+    const index = users.findIndex(u => u.id === id);
+    if (index !== -1) {
+        const deletedUser = users.splice(index, 1)[0];
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(deletedUser));
+    } else {
+        response.writeHead(404, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify({message: 'User not found'}));
+    }
+}
 
 const server = http.createServer((req, res) => {
     if (req.method === 'POST' && req.url === '/users') {
-        // Create a new user
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            const { name, email } = JSON.parse(body);
-            const newUser = { id: uuidv4(), name, email };
-            users.push(newUser);
-            res.writeHead(201, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(newUser));
-        });
+        createUser(req, res);
     } else if (req.method === 'GET' && req.url === '/users') {
-        // Read all users
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(users));
+        getUsers(req, res);
     } else if (req.method === 'GET' && req.url.startsWith('/users/')) {
-        // Read a specific user
-        const id = req.url.split('/')[2];
-        const user = users.find(u => u.id === id);
-        if (user) {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(user));
-        } else {
-            res.writeHead(404, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'User not found' }));
-        }
+        getSingleUser(req, res);
     } else if (req.method === 'PUT' && req.url.startsWith('/users/')) {
-        // Update a user
-        const id = req.url.split('/')[2];
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            const index = users.findIndex(u => u.id === id);
-            if (index !== -1) {
-                const { name, email } = JSON.parse(body);
-                users[index] = { ...users[index], name, email };
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(users[index]));
-            } else {
-                res.writeHead(404, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ message: 'User not found' }));
-            }
-        });
+        updateUser(req, res);
     } else if (req.method === 'DELETE' && req.url.startsWith('/users/')) {
-        // Delete a user
-        const id = req.url.split('/')[2];
-        const index = users.findIndex(u => u.id === id);
-        if (index !== -1) {
-            const deletedUser = users.splice(index, 1)[0];
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(deletedUser));
-        } else {
-            res.writeHead(404, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'User not found' }));
-        }
+        deleteUser(req, res);
     } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Not Found' }));
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({message: 'Not Found'}));
     }
 });
+
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
